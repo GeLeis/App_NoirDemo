787cca4a22cf882435ec8c620370219135c9074c
gelei <gelei@anve.com>
2020-07-14 12:03:02 +0800

    跳跃算法,链表反转,链表删除倒数N个,有序链表合并,最长有效括号,fib

--- TableViewDemo/HomeViewController.m
+++ TableViewDemo/HomeViewController.m
@@ -10,12 +10,18 @@
 #import "ViewController.h"
 #import "GLRunloopTaskTool.h"
 #import "NSArray+GLSafe.h"
+#import <libkern/OSAtomic.h>
 
 @interface HomeViewController ()
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (nonatomic, strong) NSTimer *timer;
 @property (nonatomic, copy) void(^clickHandle)(int index);
 @property (nonatomic, assign) BOOL taskAdded;
+@property (atomic, assign) NSInteger num;
+@property (nonatomic, strong) NSLock *lock;
+@property (weak, nonatomic) IBOutlet UIButton *enterBtn;
+@property (nonatomic, strong) CALayer *testLayer;
+- (void)hometest;
 @end
 
 @implementation HomeViewController
@@ -42,7 +48,6 @@
         NSLog(@"xxxBlock");
     }];
     [self safeStringTst];
-    
     for (int i =0 ; i < 10; i++) {
         UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 10 + 100 * i, 100, 20)];
         label.text = [NSString stringWithFormat:@"label_%d",i];
@@ -53,6 +58,55 @@
 //        NSLog(@"==");
 //    }];
 //    [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
+    [self atomicTest];
+    self.testLayer = [CALayer layer];
+    self.testLayer.backgroundColor = [UIColor redColor].CGColor;
+    self.testLayer.frame = CGRectMake(50, 100, 50, 50);
+    [self.view.layer addSublayer:self.testLayer];
+}
+
+//- (void)viewWillAppear:(BOOL)animated {
+//    [super viewWillAppear:animated];
+//    [NSThread sleepForTimeInterval:2];
+//    NSLog(@"viewWillAppear");
+//}
+//
+//- (void)viewWillLayoutSubviews {
+//    [super viewWillLayoutSubviews];
+//    [NSThread sleepForTimeInterval:2];
+//    NSLog(@"viewWillLayoutSubviews");
+//}
+//
+//- (void)viewDidLayoutSubviews {
+//    [super viewDidLayoutSubviews];
+//    [NSThread sleepForTimeInterval:2];
+//   NSLog(@"viewDidLayoutSubviews");
+//}
+//
+//- (void)viewDidAppear:(BOOL)animated {
+//    [super viewDidAppear:animated];
+//    [NSThread sleepForTimeInterval:2];
+//    [self.view bringSubviewToFront:self.enterBtn];
+//    NSLog(@"viewDidAppear");
+//}
+
+- (void)atomicTest {
+    self.lock = [[NSLock alloc] init];
+    dispatch_queue_t queue = dispatch_queue_create("test", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(queue, ^{
+        for (int i=0; i<1000; i++) {
+//            [self.lock lock];
+            self.num = self.num + 1;
+//            [self.lock unlock];
+        }
+    });
+    dispatch_async(queue, ^{
+        for (int i=0; i<1000; i++) {
+//            [self.lock lock];
+            self.num = self.num + 1;
+//            [self.lock unlock];
+        }
+    });
 }
 
 - (void)safeArrTest {
@@ -122,7 +176,268 @@
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    CGRect frame = self.testLayer.frame;
     
+    self.testLayer.frame = CGRectMake(frame.origin.x, frame.origin.y + 50, frame.size.width, frame.size.height);
+//    self.enterBtn.layer.frame = CGRectMake(frame.origin.x, frame.origin.y + 50, frame.size.width, frame.size.height);
+}
+
+//跳跃游戏
+int jump(int* nums, int numsSize){
+    int end = 0;
+    int maxP = 0;
+    int step = 0;
+    for (int i = 0; i < numsSize - 1; i++) {
+        maxP = fmaxf(maxP, nums[i] + i);
+        if (i == end) {
+            step++;
+            end = maxP;
+        }
+    }
+    return step;
+}
+
+//链表反转
+struct ListNode* reverseList(struct ListNode* head){
+    if (head == NULL) {
+        return NULL;
+    }
+    struct ListNode *node = head;
+    struct ListNode *next = NULL;
+    while (node->next) {
+        struct ListNode *tmp = node->next;
+        node->next = next;
+        next = node;
+        node = tmp;
+    }
+    node->next = next;
+    return node;
+}
+
+//删除链表倒数第二个元素
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
+    //新增一个头节点方便处理删除头部的情况
+    struct ListNode *pre = (struct ListNode *)malloc(sizeof(struct ListNode) * 1);
+    pre->next = head;
+    struct ListNode *first = pre;
+    struct ListNode *second = pre;
+    //0,1,2,   3,4,5,6
+    //s   f    f
+    while (first->next) {
+        first = first->next;
+        if (n > 0) {
+            n--;
+        } else {
+            second = second->next;
+        }
+    }
+    second->next = second->next->next;
+    return pre->next;
+}
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+//合并两个有序链表
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+    if (l1 == NULL) {
+        return l2;
+    }
+    if (l2 == NULL) {
+        return l1;
+    }
+    struct ListNode *head = (struct ListNode *)malloc(sizeof(struct ListNode) * 1);
+    struct ListNode *tmp = head;
+    while(l1 && l2) {
+        if (l1->val <= l2->val) {
+            tmp->next = l1;
+            l1 = l1->next;
+    
+        } else {
+            tmp->next = l2;
+            l2 = l2->next;
+        }
+        tmp = tmp->next;
+    }
+    if (l1) tmp->next = l1;
+    if (l2) tmp->next = l2;
+    return head->next;
+}
+
+//最长有效括号,通过栈去保存栈顶数据
+int longestValidParentheses(char * s){
+    int max= 0 ;
+    int top= -1 ;
+    int sLen = strlen(s);
+    int st[sLen+1];
+    st[++top] = -1;
+    for (int i = 0; i < sLen; i++) {
+        if (s[i] == '(') {
+            st[++top] = i;
+        } else {
+            --top;
+            if (top == -1) {
+                st[++top] = i;
+            } else {
+                max = fmax(max, i - st[top]);
+            }
+        }
+    }
+    return max;
+    
+}
+
+int* divingBoard(int shorter, int longer, int k, int* returnSize){
+    //i块短板,k-i块长板,i从k到0,短板越多,值越小
+    if (k == 0) {
+        int *result = (int *)malloc(sizeof(int) * (1));
+        result[0] = 0;
+        return result;
+    }
+    if (shorter == longer) {
+        int *result = (int *)malloc(sizeof(int) * (1));
+        result[0] = k*shorter;
+        return result;
+    };
+    int *result = (int *)malloc(sizeof(int) * (k+1));
+    for (int i = k;i >=0;i--) {
+        result[k - i] = i * shorter + (k - i) * longer;
+    }
+    return result;
+}
+//不同路径问题
+int uniquePaths(int m, int n){
+    //定一个一个二维数组
+    int **board = (int **)malloc(sizeof(int *) * n);
+    for (int i = 0; i < n; i++) {
+        board[i] = (int *)malloc(sizeof(int) * m);
+    }
+    for (int i = 0 ; i < n; i++) {
+        for (int j = 0 ; j < m; j++) {
+            if (i == 0 &&  j== 0) {
+                board[i][j]= 1;
+            } else if (i > 0 && j == 0) {
+                board[i][j] = board[i - 1][j];
+            } else if (i == 0 && j > 0) {
+                board[i][j] = board[i][j - 1];
+            } else {
+                board[i][j] = board[i - 1][j] + board[i][j - 1];
+            }
+        }
+    }
+    return board[n - 1][m - 1];
+}
+
+- (void)osspinLockThreadTest {
+    for (int i = 0; i < 100; i++) {
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            static OSSpinLock aspect_lock = OS_SPINLOCK_INIT;
+            OSSpinLockLock(&aspect_lock);
+            NSLog(@"osspinLockThreadTest i = %d",i);
+            OSSpinLockUnlock(&aspect_lock);
+        });
+    }
+}
+
+bool isMatch(char* s, char* p){
+    int slenght = (int)strlen(s);
+    int plenght = (int)strlen(p);
+    //首先判断非空
+    if (slenght == 0 || plenght == 0) {
+        return slenght+plenght == 0;
+    }
+    //长度为1
+    if (slenght == 1 && plenght == 1) {
+        return s[0] == p[0] || p[0] == '.';
+    }
+    //*存在0和多个,两种情况
+    int pIndex = 0;
+    int sIndex = 0;
+    while (sIndex < slenght && pIndex < plenght) {
+        //第一个元素必须匹配
+        if (p[pIndex] == '.' && (pIndex < plenght - 1 ? p[pIndex + 1] != '*' : true)) {
+            p[pIndex] = s[sIndex];
+        }
+        bool firstMatch = s[sIndex] == p[pIndex] || p[pIndex] == '.';
+        if (firstMatch) {
+            if (p[pIndex + 1] != '*') {
+                pIndex++;
+            }
+            sIndex++;
+        } else {
+            pIndex+=2;
+        }
+    }
+    return true;
+}
+
+int fib(int n){
+    if (n < 3) {
+        return 1;
+    }
+    int a = 1,b=1,c = 0;
+    for (int i = 3; i <=n; i++) {
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    return c;
+}
+
+int numDecodings(char * s) {
+    int i     = 0;
+    int size  = 0;
+    int res   = 0;
+    int n_1   = 0;
+    int n_2   = 0;
+    size      = strlen(s);
+    
+    /***********************************************************
+    1、s[i] = 0时，若s[i-1]是1或2，则dp[i] = dp[i-2]，否则解码出错
+    2、else if s[i-1]是1，dp[i] = dp[i-1] + dp[i-2]
+    3、else if s[i-1]是2，且s[i]小于7，dp[i] = dp[i-1] + dp[i-2]
+    4、else dp[i] = dp[i-1]
+    ***********************************************************/
+    if('0' == s[0])
+    {
+        return 0;
+    }
+
+    n_2 = 1;
+    n_1 = 1;
+    res = 1;
+
+    for(i = 1;i<size;i++)
+    {
+        if('0' == s[i])
+        {
+            if(('1' == s[i-1])||('2' == s[i-1]))
+            {
+                res = n_2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else if('1' == s[i-1])
+        {
+            res = n_2 + n_1;
+        }
+        else if(('2' == s[i-1])&&(s[i] < '7'))
+        {
+            res = n_2 + n_1;
+        }
+        else
+        {
+            res = n_1;
+        }
+        
+        n_2 = n_1;
+        n_1 = res;
+    }
+    return res;
 }
 
 - (void)taskToolTest {
